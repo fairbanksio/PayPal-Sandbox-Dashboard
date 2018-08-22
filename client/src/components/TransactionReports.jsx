d2893c4a4195008f802803feb9d35b7bef8c88da
Jon Fairbanks <fairplay89@gmail.com>
2018-08-22 00:13:20 -0700

    CSS Fixes

--- client/src/components/TransactionReports.jsx
+++ client/src/components/TransactionReports.jsx
@@ -112,7 +112,7 @@ class IpnList extends React.Component {
                     <span style={{ paddingRight: '25px' }}><b>Payment Status: </b>{item.ipnMessage.payment_status}</span>
                     <span style={{ paddingRight: '25px' }}><b>IPN Status: </b>{item.status}</span>
                     <span style={{ paddingRight: '25px' }}><b>Buyer: </b>{item.ipnMessage.payer_email + ' (' + item.ipnMessage.payer_id + ')'}</span>
-                    <span style={{ paddingRight: '25px' }}><b>Payment Amount: $</b>{item.ipnMessage.mc_gross}</span>
+                    <span style={{ paddingRight: '25px' }}><b>Payment Amount: </b>{'$' + item.ipnMessage.mc_gross}</span>
                   </Typography>
                   <hr/>
                   <div>
@@ -230,7 +230,7 @@ class TransactionReports extends React.Component {
               </ExpansionPanelSummary>
               <ExpansionPanelDetails>
                 <Typography>
-                  <hr/>
+                  <hr/><br/>
                   <CodeMirror
                     ref='dbCollection'
                     value={JSON.stringify(this.state.ipnData, null, ' ')}
