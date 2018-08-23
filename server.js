3eb739ec197496ee71f1dc48afc3345466fdb694
Jon Fairbanks <fairplay89@gmail.com>
2018-08-23 03:00:59 -0700

    Remove Timers

--- server.js
+++ server.js
@@ -72,10 +72,8 @@ app.get('/api/execute-agreement', function(req, res){
 
 app.get('/api/ipnData', function(req, res){
 	connection.db.collection("ipn", function(err, collection){
-			console.time('Loaded IPN Data from DB');
 			collection.find({}).sort({ timestamp: -1 }).limit(50).toArray(function(err, data){
 					res.json(data);
-					console.timeEnd('Loaded IPN Data from DB');
 			})
 	});
 })
