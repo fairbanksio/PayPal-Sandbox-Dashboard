653e7772c8989b339f79e810e3e4a5ee98d40a25
Jon Fairbanks <fairplay89@gmail.com>
2018-08-22 21:28:13 -0700

    New Getting Started UI

--- client/src/components/GettingStarted.jsx
+++ client/src/components/GettingStarted.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
 
 function TabContainer(props) {
   return (
@@ -14,21 +19,34 @@ function TabContainer(props) {
 }
 
 const styles = theme => ({
-  root: {
-    flexGrow: 1,
-    width: '100%',
-    backgroundColor: theme.palette.background.paper,
+  layout: {
+    width: 'auto',
+    display: 'block', // Fix IE11 issue.
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
   },
-  button: {
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+  },
+  avatar: {
     margin: theme.spacing.unit,
+    backgroundColor: '#005EA6' || theme.palette.secondary.main,
   },
-  textField: {
-    marginLeft: theme.spacing.unit,
-    marginRight: theme.spacing.unit,
-    width: 200,
+  form: {
+    width: '100%', // Fix IE11 issue.
+    marginTop: theme.spacing.unit,
   },
-  input: {
-    display: 'none',
+  submit: {
+    marginTop: theme.spacing.unit * 3,
   },
 });
 
@@ -39,7 +57,6 @@ class GettingStarted extends React.Component {
       clientID: "",
       clientSecret: "",
     };
-
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -47,6 +64,7 @@ class GettingStarted extends React.Component {
   handleChange(event) {
     // Save text value changes to state
     this.setState({[event.target.id]: event.target.value});
+    console.log('Handling Change')
   }
 
   handleSubmit(event) {
@@ -54,42 +72,54 @@ class GettingStarted extends React.Component {
     localStorage.setItem("clientID", this.state.clientID);
     localStorage.setItem("clientSecret", this.state.clientSecret);
     event.preventDefault();
+    console.log('Credentials Saved!')
   }
 
   render() {
     const { classes } = this.props;
     return (
       <TabContainer>
-        <div>
-            <h4>Getting Started in the PayPal Sandbox</h4>
-            <TextField
-              id="clientID"
-              label="Client ID"
-              type="search"
-              className={classes.textField}
-              margin="normal"
-              onChange={this.handleChange}
-              style={{width: '500px'}}
-            />
-            <br/>
-            <TextField
-              id="clientSecret"
-              label="Secret"
-              type="search"
-              className={classes.textField}
-              margin="normal"
-              onChange={this.handleChange}
-              style={{width: '500px'}}
-            />
-            <br/><br/>
-            <Button variant="outlined" color="primary" className={classes.button} onClick={this.handleSubmit}>
-              Save
-            </Button>
-          </div>
+        <React.Fragment>
+          <CssBaseline />
+          <main className={classes.layout}>
+            <Paper className={classes.paper}>
+              <Avatar className={classes.avatar}>
+                <i class="fab fa-paypal"/>
+              </Avatar>
+              <Typography variant="headline">Set Credentials</Typography>
+              <form className={classes.form}>
+                <FormControl margin="normal" required fullWidth>
+                  <InputLabel htmlFor="clientID">Client ID</InputLabel>
+                  <Input id="clientID" name="clientID" autoComplete="clientID" autoFocus onChange={this.handleChange} />
+                </FormControl>
+                <FormControl margin="normal" required fullWidth>
+                  <InputLabel htmlFor="clientSecret">Secret</InputLabel>
+                  <Input
+                    name="clientSecret"
+                    id="clientSecret"
+                    autoComplete="clientSecret"
+                    onChange={this.handleChange}
+                  />
+                </FormControl>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="outlined"
+                  color="primary"
+                  className={classes.submit}
+                  onClick={this.handleSubmit}
+                >
+                  Save
+                </Button>
+              </form>
+            </Paper>
+          </main>
+        </React.Fragment>
       </TabContainer>
     );
   }
 }
+
 GettingStarted.propTypes = {
   classes: PropTypes.object.isRequired,
 };
