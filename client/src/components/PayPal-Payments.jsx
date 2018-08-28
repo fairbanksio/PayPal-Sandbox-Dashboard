f2589190c50ba3c8d0002810aea74f98570a7bda
root <brandon.sorgdrager@gmail.com>
2018-08-28 17:05:01 +0000

    Included IPN Listener to server.js

--- client/src/components/PayPal-Payments.jsx
+++ client/src/components/PayPal-Payments.jsx
@@ -88,6 +88,10 @@ const styles = theme => ({
     width: '100%', // Fix IE11 issue.
     marginTop: theme.spacing.unit,
   },
+  formControl: {
+    margin: theme.spacing.unit,
+    minWidth: 120,
+  },
   submit: {
     marginTop: theme.spacing.unit * 3,
   },
@@ -95,7 +99,6 @@ const styles = theme => ({
     fontSize: theme.typography.pxToRem(15),
     flexShrink: 0,
     paddingRight: theme.spacing.unit * 3,
-
   },
   secondaryHeading: {
     fontSize: theme.typography.pxToRem(15),
