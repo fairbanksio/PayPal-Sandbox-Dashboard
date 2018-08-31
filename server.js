808679d9201a594000730b3285ee58711aa35bc7
Jon Fairbanks <fairplay89@gmail.com>
2018-08-30 21:23:50 -0700

    Code Comments

--- server.js
+++ server.js
@@ -10,19 +10,25 @@ var newIPN = require('./models/ipn');
 
 const path = require('path');
 
+// MongoDB Setup
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 var connection = mongoose.connection;
-
 connection.on('error', console.error.bind(console, 'DB Connection Error:'));
 connection.once('open', function () { });
 
+// Express Setup
 var serverHost = process.env.SERVER_HOST;
 var app = express();
 app.use(bodyParser.urlencoded({	extended: false }));
 app.use(bodyParser.json());
 
-// Define path for react-app client front end
+// Define Client Path
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+////
+// Payment Endpoints
+////
 
 // Create Payment
 app.post('/api/create-payment', function(req, res){
@@ -83,22 +89,10 @@ app.post('/api/execute-agreement', function(req, res){
 	})
 })
 
-app.get('/api/ipnData', function(req, res){
-  connection.db.collection("ipn", function(err, collection){
-    collection.find({}).sort({ timestamp: -1 }).limit(50).toArray(function(err, data){
-      res.json(data);
-    })
-  });
-})
 
-app.get('/api/ipnCount', function(req, res){
-	connection.db.collection("ipn", function(err, collection){
-		collection.find({}).count(function(err, data){
-			res.json(data);
-		})
-	});
-})
-
+////
+// Payment Handlers
+////
 function createPayment(apiKey, apiSecret, redirectURL, paymentJSON, callback){
 	if(apiKey && apiSecret){
 		// Configure PayPal SDK
@@ -190,7 +184,7 @@ function createBillingAgreement(apiKey, apiSecret, billingAgreementAttributes, c
 }
 
 function executePayment(apiKey, apiSecret, payerId, paymentId, callback){
-		//configure paypal
+		// Configure PayPal SDK
 		paypal.configure({
 			'mode': 'sandbox',
 			'client_id': apiKey,
@@ -214,7 +208,7 @@ function executePayment(apiKey, apiSecret, payerId, paymentId, callback){
 }
 
 function executeAgreement(apiKey, apiSecret, paymentToken, callback){
-		//configure paypal
+		// Configure PayPal SDK
 		paypal.configure({
 			'mode': 'sandbox',
 			'client_id': apiKey,
@@ -235,10 +229,12 @@ function executeAgreement(apiKey, apiSecret, paymentToken, callback){
 		});
 }
 
-app.post('/', function(req, res) {
-  // Before anything else, log the IPN
-	logger.info('New IPN Message: ' + JSON.stringify(req.body));
 
+////
+// IPN Handler and Database Management
+////
+app.post('/', function(req, res) {
+	logger.info('New IPN Message: ' + JSON.stringify(req.body)); // Before anything else, log the IPN
 	// Read the IPN message sent from PayPal and prepend 'cmd=_notify-validate'
 	req.body = req.body || {};
 	res.status(200).send('OK');
@@ -271,7 +267,7 @@ app.post('/', function(req, res) {
 		if (!error && response.statusCode === 200) {
 			// inspect IPN validation result and act accordingly
 			if (body.substring(0, 8) === 'VERIFIED') {
-				// The IPN is verified, process it
+				// VERIFIED
 				var item_name = req.body['item_name'];
 				var item_number = req.body['item_number'];
 				var payment_status = req.body['payment_status'];
@@ -280,15 +276,14 @@ app.post('/', function(req, res) {
 				var txn_id = req.body['txn_id'];
 				var receiver_email = req.body['receiver_email'];
 				var payer_email = req.body['payer_email'];
-
-				// To loop through the &_POST array and print the NV pairs to the screen:
+				// Loop through the array and print the NVPs:
 				logger.debug('IPN Data: ')
 				for (var key in req.body) {
 					var value = req.body[key];
 					logger.debug(key + "=" + value);
 				}
 			} else if (body.substring(0, 7) === 'INVALID') {
-				// IPN invalid, log for manual investigation
+				// INVALID
 				logger.error('IPN Invalid: ' + body);
 			}
 			// Save the IPN and associated data to MongoDB
@@ -305,15 +300,39 @@ app.post('/', function(req, res) {
 	});
 });
 
+// Get IPN Data from MongoDB
+app.get('/api/ipnData', function(req, res){
+  connection.db.collection("ipn", function(err, collection){
+    collection.find({}).sort({ timestamp: -1 }).limit(50).toArray(function(err, data){
+      res.json(data);
+    })
+  });
+})
+
+// Get a count of IPNs from MongoDB
+app.get('/api/ipnCount', function(req, res){
+	connection.db.collection("ipn", function(err, collection){
+		collection.find({}).count(function(err, data){
+			res.json(data);
+		})
+	});
+})
 
 
-// Serve react-app client application
+////
+// Serve Client Application
+///
 app.get('*', (req,res) => {
 	res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
-var port = null;
-if(process.env.PORT){ port = process.env.PORT; }else{ port = 3000; } // Default port is 8888 unless passed
-app.listen(port);
-var msg = 'Server listening at http://localhost:' + port;
-console.log(msg);
+
+////
+// App Launcher
+////
+var port = process.env.PORT || 3000;
+var host = process.env.REACT_APP_HOST || '0.0.0.0';
+app.listen(port, host, function() {
+	var msg = 'Server listening at http://localhost:' + port;
+	console.log(msg);
+});
