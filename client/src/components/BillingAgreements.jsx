0d6722804a7212756c897249716fd94d5ca491e0
root <brandon.sorgdrager@gmail.com>
2018-09-03 00:21:45 +0000

    Bug fix - Resolve date issues for billing agreements

--- client/src/components/BillingAgreements.jsx
+++ client/src/components/BillingAgreements.jsx
@@ -533,16 +533,17 @@ class PayPalAgreements extends React.Component {
     var billPlanId = localStorage.getItem("pBillPlanId")
     var agreementName = localStorage.getItem("agreementName")
     var agreementDesc = localStorage.getItem("agreementDesc")
-    var agreementStart = new Date(localStorage.getItem("agreementStart")).toISOString().slice(0,19) + 'Z';;
+    var agreementStart = new Date(localStorage.getItem("agreementStart"));
     var isoDate = new Date();
-    isoDate.setSeconds(isoDate.getSeconds() + 10);
-    isoDate = isoDate.toISOString().slice(0,19) + 'Z';
+    if(agreementStart < isoDate){agreementStart = isoDate}
+    agreementStart.setSeconds(agreementStart.getSeconds() + 10);
+    agreementStart = agreementStart.toISOString().slice(0,19) + 'Z';
 
     this.setState({chipStatus:'Creating billing agreement...', chipIndicator:'loading'})
     var billingAgreementAttributes = {
         "name": agreementName || "Fast Speed Agreement",
         "description": agreementDesc || "Agreement for Fast Speed Plan",
-        "start_date": agreementStart || isoDate,
+        "start_date": agreementStart ,
         "plan": {
             "id": billPlanId
         },
